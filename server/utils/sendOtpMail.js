aae479f8ed7ff206348c38e84f4801c6b2ffab4c
Keshav Singhania <worksinghania@gmail.com>
2026-08-31 14:50:32 +0530

    Add timeout settings to email transporter
    Increase connection, greeting, and socket timeouts for email transport.

--- server/utils/sendOtpMail.js
+++ server/utils/sendOtpMail.js
@@ -18,6 +18,9 @@ const sendOtpMail = async (to, otp) => {
   host: 'smtp.gmail.com',
   port: 587,
   secure: false,
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
